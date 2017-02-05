cd7621bdcfed6cb08d0f90be5a6c9db59afd9ccf
krismania <krismania1@gmail.com>
2017-02-05 23:13:02 +1100

    Added API routes for show info and episodes
    Added API routes /shows, /shows/[id] & /shows/[id]/S[season], which return
    info based on the media folder.

--- src/server/modules/api.js
+++ src/server/modules/api.js
@@ -1,11 +1,50 @@
 var router = require('express').Router();
+var fs = require('fs');
 
 router.get('/', function(req, res) {
 	res.send('Howdy');
 });
 
-router.get('/bundle.js', function(req, res) {
-	res.sendFile('bundle.js', {root: './build'});
-})
+function getShowInfo(id) {
+	var info = JSON.parse(fs.readFileSync('static/media/' + id + '/info.json'));
+	info.id = id;
+	// count seasons (count folders, subtract info.json)
+	info.seasons = fs.readdirSync('static/media/' + id).length - 1;
+
+	return info;
+}
+
+function getSeasonEps(id, season) {
+	var eps = [];
+	var readDir = fs.readdirSync('static/media/' + id + '/' + season);
+	// remove files that aren't .mp4s & remove file extension
+	readDir = readDir.filter((file) => file.slice(-3) === 'mp4')
+	                 .map((file) => parseInt(file.slice(0, -4)));
+
+	return readDir;
+}
+
+// list all available shows
+router.get('/shows', function(req, res) {
+	var shows = [];
+	var showFolders = fs.readdirSync('static/media');
+
+	showFolders.forEach(function(showFolder) {
+		shows.push(getShowInfo(showFolder));
+	});
+
+	res.json(shows);
+});
+
+// send information about the specified show
+router.get('/shows/:id', function(req, res) {
+	res.json(getShowInfo(req.params.id));
+});
+
+// list episodes in show season
+router.get('/shows/:id/S:season', function(req, res) {
+	var eps = { episodes: getSeasonEps(req.params.id, req.params.season) };
+	res.json(eps);
+});
 
 module.exports = router;
