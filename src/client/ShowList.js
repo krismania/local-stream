7a5e25b669d18ef2272c8855bffbe3df4f4b4b64
krismania <krismania1@gmail.com>
2017-02-15 20:02:24 +1100

    Dynamically change number of columns on ShowList page

--- src/client/ShowList.js
+++ src/client/ShowList.js
@@ -8,20 +8,40 @@ class ShowList extends React.Component {
 	constructor(props) {
 		super(props);
 
+		this.state = { numCols: 1 }
+
 		this.handleShowTouchTap = this.handleShowTouchTap.bind(this);
+		this.getNumCols = this.getNumCols.bind(this);
 	}
 
 	handleShowTouchTap(name) {
 		browserHistory.push('/stream/' + name);
 	}
 
+	componentWillMount() {
+		this.getNumCols();
+	}
+
 	componentDidMount() {
+		window.addEventListener('resize', this.getNumCols);
 		document.title = 'Shows - LocalStream';
+	}
+
+	getNumCols() {
+		var width = document.getElementsByTagName('body')[0].clientWidth;
+		var cols;
+		// decide based on page width
+		if (width <= 450) cols = 1;
+		else if (width <= 670) cols = 2;
+		else cols = 3;
+
+		console.log(cols);
+		this.setState({ numCols: cols });
 	}
 
 	render() {
 		return(
-			<GridList cellHeight={180} cols={4}>
+			<GridList cellHeight={180} cols={this.state.numCols}>
 				<Subheader>Shows</Subheader>
 				{this.props.shows.map((show) => (
 					<GridTile
